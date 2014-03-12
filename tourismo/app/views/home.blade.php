89ca33813d9247f89e089604b6768bd5703ba9a3
Marko Andjelkovic <andjelko85@gmail.com>
2014-03-12 21:44:40 +0100

    metro css & js for win8 look

--- tourismo/app/views/home.blade.php
+++ tourismo/app/views/home.blade.php
@@ -5,8 +5,14 @@
 	<title>Tourismo</title>
 	{{HTML::style('bootstrap/css/bootstrap.css')}}
 	{{HTML::style('bootstrap/css/bootstrap-responsive.css')}}
+	{{HTML::style('bootstrap/css/metro-bootstrap.css')}}
+	{{HTML::style('bootstrap/css/metro-bootstrap-responsive.css')}}
 	{{HTML::script('bootstrap/js/bootstrap.js')}}
-	
+	{{HTML::script('bootstrap/js/jquery-2.1.0.min.js')}}
+	{{HTML::script('bootstrap/js/jquery.widget.min.js')}}
+	{{HTML::script('bootstrap/js/jquery-ui.js')}}
+	{{HTML::script('bootstrap/js/jquery.ui.widget.js')}}
+	{{HTML::script('bootstrap/js/metro-loader.js')}}
 </head>
 <body>
 	<div id='header' class="row">
@@ -18,7 +24,7 @@
 			@endif
 		</div>
 	</div>
-    <div id="content" class="row">
+    <div id="content" class="metro">
         @yield('content')
        </div>
 
--- tourismo/app/views/homepage.blade.php
+++ tourismo/app/views/homepage.blade.php
@@ -3,25 +3,48 @@
 @section('content')
 
 <div class="container">
-<div class='row'>
-	<div id='rezervacije' class="span4">
-		{{HTML::linkRoute('reservations', 'Rezervacije')}}
+	<!-- <div class='row'>
+		<div id='rezervacije' class="span4, tile double">
+			{{HTML::linkRoute('reservations', 'Rezervacije')}}
+		</div>
+		<div id='putnici' class="span4, tile double">
+			{{HTML::linkRoute('passangers', 'Putnici')}}
+		</div>
+		<div id='destinacije' class="span4, tile double">
+			{{HTML::linkRoute('destinations', 'Destinacije')}}
+		</div>
 	</div>
-	<div id='putnici' class="span4">
-		{{HTML::linkRoute('passangers', 'Putnici')}}
+	<div class="row">
+		<div id='aranzmani' class="span4, tile double">Aranžmani</div>
+		<div id='placanja' class="span4, tile double">Plaćanja</div>
+		<div id='izvestaji' class="span4, tile double">Izveštaji</div>
 	</div>
-	<div id='destinacije' class="span4">
-		{{HTML::linkRoute('destinations', 'Destinacije')}}
+	<div class="row">
+		<div id='korisnici' class="span4, tile double">Korisnici</div>
+	</div> -->
+	<div class="tile-area">
+	    <div class="tile-group six">
+	       <a href="reservations">
+	        <div class="tile double live bg-lightBlue" data-role="live-tile" data-effect="slideUp" data-click="transform">
+	          <div class="tile-content"><center><p>Rezervacije</p></center></div>
+	          <div class="tile-content"><center><p>Reservations</p></center></div>
+	        </div>
+	      </a>
+	      <a href="passangers">
+	        <div class="tile double live bg-lightBlue" data-role="live-tile" data-effect="slideUp" data-click="transform">
+	          <div class="tile-content"><center><p>Putnici</p></center></div>
+	          <div class="tile-content"><center><p>Passangers</p></center></div>
+	        </div>
+	      </a>
+	      <a href="destinations">
+	        <div class="tile double live bg-lightBlue" data-role="live-tile" data-effect="slideUp" data-click="transform">
+	          <div class="tile-content"><center><p>Destinacije</p></center></div>
+	          <div class="tile-content"><center><p>Destinations</p></center></div>
+	        </div>
+	      </a>
+		</div>
 	</div>
-</div>
-<div class="row">
-	<div id='aranzmani' class="span4">Aranžmani</div>
-	<div id='izvestaji' class="span4">Izvestaji</div>
-	<div id='korisnici' class="span4">Korisnici</div>
-</div>
-<div class="row">
-	<div id='placanja' class="span4">Placanja</div>
 </div>
 
-</div>
+
 @stop
